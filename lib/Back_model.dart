d4f2dc7a4aa701257d4e0e24c625e1f8c0dceae6
Julie Douangdara <douangdaraj19@students.ecu.edu>
2023-03-15 18:56:42 -0400

    Updated buttons on the back model

--- lib/Back_model.dart
+++ lib/Back_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math' as angles;
 class Back_model extends StatelessWidget {
   const Back_model({super.key});
 
@@ -28,22 +28,177 @@ class Back_model extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
+                  // TRAPS BUTTON
                   Positioned(
-                    top: 50,
-                    left: 100,
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      color: Colors.red,
+                    top: 35,
+                    left: 90,
+                    child: Transform.rotate(
+                      angle: 45 * angles.pi / 180,
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(2.0),
+                          )
+                      ),
                     ),
                   ),
+                  // LOWER BACK BUTTON
                   Positioned(
-                    top: 150,
-                    left: 200,
+                    top: 165,
+                    left: 122,
+                    child: Transform.rotate(
+                      angle: 45 * angles.pi / 180,
+                      child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            color: Colors.pink,
+                            borderRadius: BorderRadius.circular(2.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // LATS BUTTON LEFT
+                  Positioned(
+                    top: 115,
+                    left: 85,
+                    child: Transform.rotate(
+                      angle: -45 * angles.pi / 180,
+                      child: Container(
+                          width: 35,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius: BorderRadius.circular(20.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // LATS BUTTON RIGHT
+                  Positioned(
+                    top: 115,
+                    left: 160,
+                    child: Transform.rotate(
+                      angle: 45 * angles.pi / 180,
+                      child: Container(
+                          width: 35,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius: BorderRadius.circular(20.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // TRICEP BUTTON RIGHT
+                  Positioned(
+                    top: 100,
+                    left: 213,
+                    child: Transform.rotate(
+                      angle: -25 * angles.pi / 180,
+                      child: Container(
+                          width: 37,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // TRICEP BUTTON LEFT
+                  Positioned(
+                    top: 100,
+                    left: 30,
+                    child: Transform.rotate(
+                      angle: 25 * angles.pi / 180,
+                      child: Container(
+                          width: 39,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // CALVES BUTTON RIGHT
+                  Positioned(
+                    top: 340,
+                    left: 170,
+                    child: Transform.rotate(
+                        angle: -10 * angles.pi / 180,
+                      child: Container(
+                       width: 45,
+                       height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.brown,
+                          borderRadius: BorderRadius.circular(20.0),
+                        )
+                    ),
+                  ),
+                  ),
+                  // CALVES BUTTON LEFT
+                  Positioned(
+                    top: 340,
+                    left: 68,
+                    child: Transform.rotate(
+                      angle: 10 * angles.pi / 180,
+                      child: Container(
+                          width: 45,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.brown,
+                            borderRadius: BorderRadius.circular(20.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // HAMSTRING BUTTON LEFT
+                  Positioned(
+                    top: 260,
+                    left: 80,
+                    child: Transform.rotate(
+                      angle: 10 * angles.pi / 180,
+                      child: Container(
+                          width: 50,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(15.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // HAMSTRING BUTTON RIGHT
+                  Positioned(
+                    top: 260,
+                    left: 155,
+                    child: Transform.rotate(
+                      angle: -10 * angles.pi / 180,
+                      child: Container(
+                          width: 50,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(15.0),
+                          )
+                      ),
+                    ),
+                  ),
+                  // GLUTES BUTTON
+                  Positioned(
+                    top: 210,
+                    left: 90,
                     child: Container(
-                      width: 50,
+                      width: 110,
                       height: 50,
-                      color: Colors.green,
+                      decoration: BoxDecoration(
+                      color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(40.0),
+                      )
                     ),
                   ),
                   // Add more Positioned widgets for other clickable parts of the image
