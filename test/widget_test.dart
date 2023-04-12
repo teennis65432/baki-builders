aba06b1318108c7d4c3e5f966bed106d8b4a58ef
EpicmanTdog <tdogennis@gmail.com>
2023-04-12 17:47:51 -0400

    changed testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:baki_builders/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:baki_builders/MainHomePage.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-
+  test('Firebase shouldnt be null', () {
+    DefaultFirebaseOptions dfb = new DefaultFirebaseOptions();
+    Object j = dfb.runtimeType;
+    expect(dfb, isNotNull);
   });
 }
